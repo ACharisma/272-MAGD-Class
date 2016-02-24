8534df0eb47b5c3c6c83e1e9517badb3e7c0cf37
ACharisma <erlwigaj17@uww.edu>
2016-02-24 10:25:35 -0600

    g

---
+++ Assets/scripts/scoreModifier.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+public class scoreModifier : MonoBehaviour
+{
+    public Text scoreTargetText;
+
+
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Q))
+        {
+            statsManager.score++;
+            Debug.Log(statsManager.score);
+            scoreTargetText.text = statsManager.score.ToString();
+        }
+
+    }
+}
---
+++ Assets/scripts/statsManager.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class statsManager : MonoBehaviour {
+
+    public static int lives;
+    public static int charisma;
+    public static int score;
+    public static float health;
+    public static string userName;
+    public static int questionIndex;
+    public static int answerIndex;
+
+}
